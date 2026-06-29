3930112c56867d178df8bed8174aa3b85aedf665
User <User@DESKTOP-QHG18FL>
2026-06-29 12:51:20 +0300

    Intro

--- ReadMe.txt
+++ ReadMe.txt
@@ -1,2 +1,6 @@
 ﻿https://github.com/okovtun/P_418_WIN.git
 https://www.youtube.com/playlist?list=PLeC3gh-CKRG4
+
+TODO:
+1. Прислать в жернал ссылку на репозиторий;
+2. Добавить в окно значек;
--- WinAPI/DialogBox/resource.h
+++ WinAPI/DialogBox/resource.h
@@ -3,6 +3,11 @@
 // Used by Resource.rc
 //
 #define IDD_DIALOG1                     101
+#define IDC_STATIC_LOGIN                1001
+#define IDC_STATIC_PASSWORD             1002
+#define IDC_EDIT_LOGIN                  1003
+#define IDC_EDIT2                       1004
+#define IDC_EDIT_PASSWORD               1004
 
 // Next default values for new objects
 // 
@@ -10,7 +15,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        103
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_CONTROL_VALUE         1005
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
