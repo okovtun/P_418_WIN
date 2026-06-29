79c5dad784b8a6aa562e4b3a4bb74a9afad8ecac
User <User@DESKTOP-QHG18FL>
2026-06-29 12:32:43 +0300

    DlgProc: EndDialog DONE

---
+++ WinAPI/DialogBox/main.cpp
@@ -0,0 +1,26 @@
+﻿#include<Windows.h>
+#include"resource.h"
+
+//Прототип функции:
+BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
+{
+	DialogBoxParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), NULL, DlgProc, NULL);
+}
+
+//Реализация функции:
+BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	case WM_INITDIALOG:	//Отрабатывает один раз, при запеске окна
+		break;
+	case WM_COMMAND:	//Обработка нажатия кнопок, действий мышью, и т.д.
+		break;
+	case WM_CLOSE:		//Отрабатывает при нажатии на кнопку закрытия окна 'X'
+		EndDialog(hwnd, 0);
+		break;
+	}
+	return FALSE;
+}
---
+++ WinAPI/WinAPI/main.cpp
@@ -0,0 +1,23 @@
+﻿#include<Windows.h>
+
+INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
+{
+	/*
+	---------------------------
+	GUI - Graphics User Interface (Графический пользовательский интерфейс);
+	1. Окно сообщения	- MessageBox;
+	2. Окно диалога		- DialogBox;
+	3. Главное окно прилодения - MainWindow;
+	Флаги - это биты, которые включают/отключают некоторые возможности.
+	Модальным называется окно, которое блокироует своего родителя.
+	---------------------------
+	*/
+	MessageBox
+	(
+		NULL,	//Родительское окно
+		"Hello WinAPI!\nРазработка настольных прилодений для Windows.",//Сообщение, выводимое пользователю
+		"Hello",//Заголовок окна
+		//Флаги, которые определяют значек, выводимый в окне, набор кнопок, и некоторый дополнительные параметры
+		MB_YESNOCANCEL | MB_HELP | MB_ICONQUESTION | MB_DEFBUTTON3 | MB_SYSTEMMODAL
+	);
+}
