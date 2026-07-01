ae6220728eca5d2d73cfc63c2dc13b01b5b9c51d
User <User@DESKTOP-QHG18FL>
2026-07-01 10:36:26 +0300

    Exit DONE

--- WinAPI/DialogBox/main.cpp
+++ WinAPI/DialogBox/main.cpp
@@ -17,6 +17,11 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	case WM_INITDIALOG:	//Отрабатывает один раз, при запеске окна
 		break;
 	case WM_COMMAND:	//Обработка нажатия кнопок, действий мышью, и т.д.
+		switch (LOWORD(wParam))
+		{
+		case IDOK: MessageBox(hwnd, "Была нажата кнопка OK!", "Info", MB_OK | MB_ICONINFORMATION); break;
+		case IDCANCEL: EndDialog(hwnd, 0);
+		}
 		break;
 	case WM_CLOSE:		//Отрабатывает при нажатии на кнопку закрытия окна 'X'
 		EndDialog(hwnd, 0);
