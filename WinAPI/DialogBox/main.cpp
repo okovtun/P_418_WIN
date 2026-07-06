f9d9b3e9c78371e5aced96e303966336292223e9
User <User@DESKTOP-QHG18FL>
2026-07-06 12:53:37 +0300

    Assembled Magda Book

--- WinAPI/DialogBox/main.cpp
+++ WinAPI/DialogBox/main.cpp
@@ -1,6 +1,8 @@
 ﻿#include<Windows.h>
 #include"resource.h"
 
+CONST CHAR g_sz_INVITE[] = "Введите имя пользователя";
+
 //Прототип функции:
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
@@ -12,12 +14,49 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, IN
 //Реализация функции:
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
+	/*
+	------------------------------------------------
+	HWND hwnd - Handler to Window. 
+	Дескриптор окна - переменная, через которую можно обратиться к окну.
+	GetDlgItem(hwnd, IDC_);
+	hwnd - родительское окно, на котором расположен элемент управления;
+	IDC_ - это ResourceID элемента управление, которые назначается в дизайнере:
+			IDC_EDIT_LOGIN;
+			IDC_EDIT_PASSWORD;
+			IDC_BUTTON_COPY;
+	------------------------------------------------
+	UINT uMsg - Message.
+	Сообщение, при помощи которого мы просим окно что-то сделать, либо спрашиваем у него какую-то информацию,
+	например:
+		- чтобы задать текст окна, нужно отправить ему сообщение WM_SETTEXT;
+		- чтобы получить текст окна, нужно отправить ему сообщение WM_GETTEXT;
+		- чтобы задать иконку окна, нужно отправить ему сообщение WM_SETICON;
+		WM_ - Window Message. Применимы к любым окнам;
+		EM_ - Edit Message. Применимо только к окнам EditControl (текстовое поле);
+		EN_ - Edit Notification. Уведомление от EditControl;
+		Сообщения - это святая святых Windows. Каждую секунду Windows отправляет тысячи сообщений.
+		Сообщения - это основное средство коммуникации между окнами.
+		Для того чтобы отправить сообщение окну используется функция SendMessage(),
+		Которая, так же как и процедура окна всегда принимает 4 параметра:
+		(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+		uMsg - uMessage (u - Unsigned, UINT - Unsigned INT, т.е. беззнаковый - только положительные числа)
+	------------------------------------------------
+	WPARAM wParam, LPARAM lParam - параметры СООБЩЕНИЯ,
+	они всегда зависят от самого сообщения,
+	у каждого сообщения свои параметры.
+	WPARAM и LPARAM занимают 4 Байта и часто делятся на HIWORD и LOWORD.
+	HIWORD - старшее слово;
+	LOWORD - младшее слово;
+	------------------------------------------------
+	*/
 	switch (uMsg)
 	{
 	case WM_INITDIALOG:	//Отрабатывает один раз, при запеске окна
 	{
 		HICON hIcon = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_ICON1));
 		SendMessage(hwnd, WM_SETICON, 0, (LPARAM)hIcon);
+		HWND hEditLogin = GetDlgItem(hwnd, IDC_EDIT_LOGIN);	//Получаем дескриптор текстового поля 'Login', для того чтобы отправить ему сообщение.
+		SendMessage(hEditLogin, WM_SETTEXT, 0, (LPARAM)g_sz_INVITE);
 	}
 		break;
 	case WM_COMMAND:	//Обработка нажатия кнопок, действий мышью, и т.д.
--- WinAPI/DialogBox/resource.h
+++ WinAPI/DialogBox/resource.h
@@ -9,6 +9,8 @@
 #define IDC_EDIT_LOGIN                  1003
 #define IDC_EDIT_PASSWORD               1004
 #define IDC_BUTTON_COPY                 1005
+#define IDC_EDIT1                       1006
+#define IDC_EDIT_CHTOTOTAM              1006
 
 // Next default values for new objects
 // 
@@ -16,7 +18,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        104
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1006
+#define _APS_NEXT_CONTROL_VALUE         1007
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
