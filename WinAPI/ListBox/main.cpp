cd558490184a9532d26148004d05bf6bdc0fb72b
User <User@DESKTOP-QHG18FL>
2026-09-07 12:09:11 +0300

    Add element DONE

--- WinAPI/ListBox/main.cpp
+++ WinAPI/ListBox/main.cpp
@@ -7,6 +7,7 @@ CONST CHAR* g_sz_VALUES[] = { "This", "is", "my", "first", "List", "Box" };
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 //DlgProc - это процедура окна, она обрабатывает любые действия пользователя.
 //Процедура окна - это самая обычная функция, которая неявно вызывается при запуске окна.
+BOOL CALLBACK DlgProcAdd(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
 {
@@ -29,6 +30,9 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	{
 		switch (LOWORD(wParam))
 		{
+		case IDC_BUTTON_ADD:
+			DialogBoxParam(GetModuleHandle(NULL), MAKEINTRESOURCE(IDD_DIALOG_ADD), hwnd, DlgProcAdd, 0);
+			break;
 		case IDOK:
 		{
 			HWND hListBox = GetDlgItem(hwnd, IDC_LIST_BOX);
@@ -39,11 +43,46 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 			sprintf(sz_message, "Вы выбрали вариант №%i со значением '%s'.", i, sz_buffer);
 			MessageBox(hwnd, sz_message, "Выбранный элемент", MB_OK | MB_ICONINFORMATION);
 		}
-			break;
+		break;
 		case IDCANCEL:EndDialog(hwnd, 0);
 		}
 	}
+	break;
+	case WM_CLOSE:EndDialog(hwnd, 0);
+	}
+	return FALSE;
+}
+BOOL CALLBACK DlgProcAdd(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	case WM_INITDIALOG:
+		SetFocus(GetDlgItem(hwnd, IDC_EDIT_ELEMENT));
 		break;
+	case WM_COMMAND:
+	{
+		switch (LOWORD(wParam))
+		{
+		case IDOK:
+		{
+			CHAR sz_buffer[256] = {};
+			HWND hEditElement = GetDlgItem(hwnd, IDC_EDIT_ELEMENT);
+			SendMessage(hEditElement, WM_GETTEXT, 256, (LPARAM)sz_buffer);
+
+			HWND hParent = GetParent(hwnd);
+			HWND hListBox = GetDlgItem(hParent, IDC_LIST_BOX);
+			if (SendMessage(hListBox, LB_FINDSTRINGEXACT, -1, (LPARAM)sz_buffer) == LB_ERR)
+				SendMessage(hListBox, LB_ADDSTRING, 0, (LPARAM)sz_buffer);
+			else
+			{
+				MessageBox(hwnd, "Такой элемент уже есть в списке", "Info", MB_OK | MB_ICONINFORMATION);
+				break;
+			}
+		}
+		case IDCANCEL:EndDialog(hwnd, 0);
+		}
+	}
+	break;
 	case WM_CLOSE:EndDialog(hwnd, 0);
 	}
 	return FALSE;
