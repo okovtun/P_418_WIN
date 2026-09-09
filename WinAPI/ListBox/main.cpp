a369a58dc77a9d3d1ac518c912b3d89a3be66b42
User <User@DESKTOP-QHG18FL>
2026-09-09 11:05:10 +0300

    ListBox DONE

--- WinAPI/ListBox/main.cpp
+++ WinAPI/ListBox/main.cpp
@@ -29,6 +29,7 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	break;
 	case WM_COMMAND:
 	{
+		HWND hListBox = GetDlgItem(hwnd, IDC_LIST_BOX);
 		switch (LOWORD(wParam))
 		{
 		case IDC_LIST_BOX:
@@ -38,9 +39,11 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		case IDC_BUTTON_ADD:
 			DialogBoxParam(GetModuleHandle(NULL), MAKEINTRESOURCE(IDD_DIALOG_ADD), hwnd, DlgProcAdd, 0);
 			break;
+		case IDC_BUTTON_DELETE:
+			SendMessage(hListBox, LB_DELETESTRING, SendMessage(hListBox, LB_GETCURSEL, 0, 0), 0);
+		break;
 		case IDOK:
 		{
-			HWND hListBox = GetDlgItem(hwnd, IDC_LIST_BOX);
 			INT i = SendMessage(hListBox, LB_GETCURSEL, 0, 0);	//Get Current Selection
 			CHAR sz_buffer[256] = {};
 			SendMessage(hListBox, LB_GETTEXT, i, (WPARAM)sz_buffer);
