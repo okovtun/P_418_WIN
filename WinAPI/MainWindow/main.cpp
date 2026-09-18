a8a71abf8dec309c171cefb5094aa7e906248165
User <User@DESKTOP-QHG18FL>
2026-09-18 11:21:30 +0300

    Lesson 1 - Window Size and Position DONE

--- WinAPI/MainWindow/main.cpp
+++ WinAPI/MainWindow/main.cpp
@@ -1,4 +1,6 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include<Windows.h>
+#include<cstdio>
 
 CONST CHAR g_szMyWindowClass[] = "My Window Class";
 INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
@@ -103,6 +105,7 @@ INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
 	switch (uMsg)
 	{
+		//WM_ - Window Message
 	case WM_CREATE:
 	{
 		HWND hStatic = CreateWindowEx
@@ -148,6 +151,25 @@ INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 			GetModuleHandle(NULL),
 			NULL
 		);
+	}
+	break;
+	case WM_SIZE:
+	case WM_MOVE:
+	{
+		RECT window_rect;
+		GetWindowRect(hwnd, &window_rect);
+		INT width = window_rect.right - window_rect.left;
+		INT height = window_rect.bottom - window_rect.top;
+		CHAR sz_buffer[256] = {};
+		sprintf
+		(
+			sz_buffer,
+			"%s, Size: %ix%i; Position: %ix%i;",
+			g_szMyWindowClass,
+			width, height,
+			window_rect.left, window_rect.top
+		);
+		SendMessage(hwnd, WM_SETTEXT, 0, (LPARAM)sz_buffer);
 	}
 	break;
 	case WM_COMMAND:
